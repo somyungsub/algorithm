24871ab0a93c9c9448410e69c730f5bc233f4a06
somyungsub <gkdldy5@naver.com>
2018-06-13 22:47:16 +0900

    Add : 코딩도장 추가

---
+++ src/codingdojang/anyFang.java
@@ -0,0 +1,36 @@
+package codingdojang;
+
+import java.util.Scanner;
+
+public class anyFang {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[][] arr = new int[][] {
+            {
+                2, 4, 1, 2, 1
+            },
+            {
+                3, 4, 2, 3, 3
+            },
+            {
+                2, 4, 1, 2, 2
+            },
+            {
+                4, 4, 4, 1, 2
+            },
+            {
+                4, 2, 3, 3, 2
+            }
+        };
+
+        int right = 0;
+
+
+
+    }
+
+    private static  boolean isTrue(){
+        return false;
+    }
+}
